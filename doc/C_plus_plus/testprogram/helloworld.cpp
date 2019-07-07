68316bd498257d08e258b65ab451262b6d260bd9
proffy2122github <52324795+proffy2122github@users.noreply.github.com>
2019-07-07 17:07:56 +0700

    Update helloworld.cpp

--- doc/C_plus_plus/testprogram/helloworld.cpp
+++ doc/C_plus_plus/testprogram/helloworld.cpp
@@ -14,5 +14,4 @@ int main(int argc, char **argv)
   _getch; // or _getch();
 }
 /* Note: in 2020, the getch command standalone must be typed _getch or getch();. Otherwise Visual Studio 2019 will fail to build the project with C4996 error */
-/* Error	C4996	 'getch': The POSIX name for this item is deprecated. Instead, use the ISO C and C++ conformant name: _getch. See online help for details.	Project2	C:\Users\Administrator\source\repos\Project2\Project2\Source.cpp	8	
-*/
+/* Error	C4996	 'getch': The POSIX name for this item is deprecated. Instead, use the ISO C and C++ conformant name: _getch. */
