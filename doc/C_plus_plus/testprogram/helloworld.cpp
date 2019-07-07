c443efa89b89fff972bf552b3a2de40cf22eb800
proffy2122github <52324795+proffy2122github@users.noreply.github.com>
2019-07-07 22:10:58 +0700

    Update helloworld.cpp

--- doc/C_plus_plus/testprogram/helloworld.cpp
+++ doc/C_plus_plus/testprogram/helloworld.cpp
@@ -2,9 +2,9 @@
 Print 1 line of characters (Hello World!) and stop the machine to see the result.
 */
 // github.com/proffy2122github/testrepo
-#include <stdio.h>
-#include <iostream>
-#include <conio.h>
+#include <stdio.h> // contains printf("");
+#include <iostream> // Including <iostream> automatically will also includes <ios>, <streambuf>, <istream>, <ostream> and <iosfwd>, sound like Apple ios.
+#include <conio.h> // contains getch(); command
 // all of the library for this file.
 
 int main(int argc, char **argv)
@@ -13,5 +13,6 @@ int main(int argc, char **argv)
   printf("Hello World!\n");
   _getch; // or _getch();
 }
-/* Note: in 2020, the getch command standalone must be typed _getch or getch();. Otherwise Visual Studio 2019 will fail to build the project with C4996 error */
+/* Note: in 2020, the getch command must be typed _getch or _getch();. Otherwise Visual Studio 2019 will fail to build the project with C4996 error */
 /* Error	C4996	 'getch': The POSIX name for this item is deprecated. Instead, use the ISO C and C++ conformant name: _getch. */
+// Compiled and ran in Visual Studio 2019 environment.
